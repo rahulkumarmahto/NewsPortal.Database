5011fae64e81d885929a0ba58dd58997a06444d2
Rahul Kumar Mahto <rahulkumarmahto@hotmail.com>
2024-05-25 12:29:30 +0530

    NewsArticle and NewsCategory table

---
+++ NewsPortal.Database/NewsPortal.Database/dbo/Tables/NewsArticle.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[NewsArticle]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] VARCHAR(MAX) NULL, 
+    [Description] VARCHAR(MAX) NULL, 
+    [NewsCategoryId] VARCHAR(MAX) NULL, 
+    [CreatedBy] VARCHAR(500) NULL, 
+    [CreatedDatetime] NCHAR(10) NULL, 
+    [ModifiedBy] VARCHAR(500) NULL, 
+    [ModifiedDatetime] NCHAR(10) NULL
+)
---
+++ NewsPortal.Database/NewsPortal.Database/dbo/Tables/NewsCategory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[NewsCategory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Name] VARCHAR(MAX) NULL, 
+    [CreatedBy] VARCHAR(500) NULL, 
+    [CreatedDatetime] NCHAR(10) NULL, 
+    [ModifiedBy] VARCHAR(500) NULL, 
+    [ModifiedDatetime] NCHAR(10) NULL
+)

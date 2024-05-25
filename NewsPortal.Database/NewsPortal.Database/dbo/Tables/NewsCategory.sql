c4937df08447ed80d7670f55b97d209f78831ed1
Rahul Kumar Mahto <rahulkumarmahto@hotmail.com>
2024-05-25 13:09:14 +0530

    Update NewsCategory.sql

--- NewsPortal.Database/NewsPortal.Database/dbo/Tables/NewsCategory.sql
+++ NewsPortal.Database/NewsPortal.Database/dbo/Tables/NewsCategory.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[NewsCategory]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-	[Name] VARCHAR(MAX) NULL, 
-    [CreatedBy] VARCHAR(500) NULL, 
-    [CreatedDatetime] NCHAR(10) NULL, 
+	[Name] VARCHAR(MAX) NOT NULL, 
+    [CreatedBy] VARCHAR(500) NOT NULL, 
+    [CreatedDatetime] DATETIME NOT NULL, 
     [ModifiedBy] VARCHAR(500) NULL, 
-    [ModifiedDatetime] NCHAR(10) NULL
+    [ModifiedDatetime] DATETIME NULL
 )
